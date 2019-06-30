0f452f59989a094c88e2f4a69ff5d44d3334f1db
Anton-Zybailo <anton_zybailo@mail.ru>
2019-06-30 23:24:53 +0300

    Мой проект с прошлого курса без подсчета лайков, анимации и кастомизации коллекций. Нужно все доделать!

---
+++ Controllers/PhotosViewCollectionController.swift
@@ -0,0 +1,87 @@
+//
+//  PhotosViewController.swift
+//  FirstProject
+//
+//  Created by Антон on 19/05/2019.
+//  Copyright © 2019 Антон. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewCollectionController: UICollectionViewController {
+
+
+    
+    var photoTitle: String = ""
+        
+    var photos: [FriendModel] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Заголовок с FriendsController
+        title = photoTitle
+        
+        
+    }
+
+    // MARK: UICollectionViewDataSource
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as! PhotosCell
+    
+//        cell.photoImage.image = photos[indexPath.row].image
+        cell.photoImage.image = photos[indexPath.row].photo
+        return cell
+    }
+
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using [segue destinationViewController].
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
