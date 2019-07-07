cabd1ff82e6a21ad4c09357bd5da5c52c99ef242
Anton-Zybailo <anton_zybailo@mail.ru>
2019-07-07 23:51:42 +0300

    установил cocoapods, добавил pod alamofire, сделал авторизацию через VK (создал доп файл для Session), добавил несколько get-запросов.

--- Controllers/LoginFormController.swift
+++ Controllers/LoginFormController.swift
@@ -117,15 +117,3 @@ class LoginFormController: UIViewController {
     
 }
 
-
-// singleton для хранения данных о текущей сессии
-
-class Session {
-    
-    static let instance = Session()
-    
-    private init () {}
-    
-    var token: String = ""
-    var userId: Int = 0
-}
